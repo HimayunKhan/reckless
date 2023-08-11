092850d6cd93be1993c9e7bb8678726b0f05202e
HimayunKhan <khanhimayu2@gmail.com>
2023-08-12 01:48:54 +0530

    16

--- components/layouts/Search.jsx
+++ components/layouts/Search.jsx
@@ -23,7 +23,7 @@ const Search = () => {
 
   return (
     <>
-      <div className="flex  h-10 hidden md:inline-flex w-[40%] items-center justify-between relative">
+      <div className="flex  h-10  md:inline-flex w-[40%] items-center justify-between relative">
         <input
           onChange={handleSearch}
           value={searchQuery}
